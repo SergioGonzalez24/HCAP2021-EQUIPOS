058480ad234f9bd84bad27a17ccb0e58e96ffc94
SergioGonzalez24 <sergiomanuel2020@gmail.com>
2021-03-19 12:59:01 -0600

    Creacion y Update de MaxPooling.py y convolucion.py

--- MaxPooling.py
+++ MaxPooling.py
@@ -1,30 +1,45 @@
 import numpy as np
 import cv2
-def MaxPoolingDos(Img):
-    fr=len(Img)//2
-    cr=len(Img[0])//2
-    Resultado=np.zeros((fr,cr),np.uint8)
-    
+def maxpoolGlobal(res):
 
-    #Proceso del maxPooling
-    a=0
-    for i in range(0,len(Img),2):
-        b=0
-        for j in range(0,len(Img),2):
-            Resultado[a][b]=np.amax(Img[i:i+2,j:j+2])
-            b+=1
-        a+=1
-    return Resultado
-Img=cv2.imread('002GS.jpeg')
-Img2=cv2.imread('003GS.jpeg')
-Img3=cv2.imread('008GS.jpeg')
-Img4=cv2.imread('011GS.jpeg')
-R1=MaxPoolingDos(Img)
-R2=MaxPoolingDos(Img2)
-R3=MaxPoolingDos(Img3)
-R4=MaxPoolingDos(Img4)
-print(R1)
-print(R2)
-print(R3)
-print(R4)
+    def MaxPoolingDos(Img):
+        fr=len(Img)//2
+        cr=len(Img[0])//2
+        Resultado=np.zeros((fr,cr),np.uint8)
+        
 
+        #Proceso del maxPooling
+        a=0
+        for i in range(0,len(Img),2):
+            b=0
+            for j in range(0,len(Img),2):
+                Resultado[a][b]=np.amax(Img[i:i+2,j:j+2])
+                b+=1
+            a+=1
+        return Resultado
+
+    Img=cv2.imread('002.jpeg')
+    Img2=cv2.imread('003GS.jpeg')
+    Img3=cv2.imread('008GS.jpeg')
+    Img4=cv2.imread('011GS.jpeg')
+
+
+    if res == 1:
+        R1=MaxPoolingDos(Img)
+        #print(R1)
+
+    elif res == 2:
+        R2=MaxPoolingDos(Img2)
+        #print(R2)
+    elif res == 3:
+        R3=MaxPoolingDos(Img3)
+        #print(R3)
+
+    elif res == 4:
+        R4=MaxPoolingDos(Img4)
+        #print(R4)
+'''
+try:
+    maxpoolGlobal(1)
+
+'''
--- convolucion.py
+++ convolucion.py
@@ -1,5 +1,42 @@
 import numpy as np
 import cv2
+from MaxPooling import maxpoolGlobal
 
-def convolucion(original,kernel):
+def convolucion(original,Kernel):
+    fr=len(original)-(len(Kernel)-1)
+    cr=len(original[0])-(len(Kernel[0])-1)
+    Resultado=np.zeros((fr,cr),np.uint8)
+    #For para recorrer filas
+    for i in range(len(Resultado)):
+        #For para recorrer columnas
+        for j in range(len(Resultado[0])):
+            suma=0
+            #hace las multiplicaciones y las suma
+            for m in range(len(Kernel)):
+                for n in range(len(Kernel[0])):
+                    suma+=Kernel[m][n]*original[m+i][n+j]
+            if suma<=255:        
+                Resultado[i][j]=round(suma)
+            else:
+                Resultado[i][j]=255
+    return Resultado
 
+'''
+#imagenes
+K=[[-1,0,1],[-1,0,1],[-1,0,1]]
+I=[[2,0,1,1,1],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]
+'''
+#imagenes a numpy arrays
+In = np.array([[2,2,4,2,2],[1,1,2,1,1],[0,0,0,0,0],[-1,-1,-2,-1,-1],[-2,-2,-4,-2,-2]])
+Kn = 1/256*np.array([[1,4,6,4,1],[4,16,24,16,4],[6,24,36,24,6],[4,16,24,16,4],[1,4,6,2,1]])
+'''
+IRGB=cv2.imread('002.jpeg')
+IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
+print(IGS.shape)
+'''
+#funcion de convolucion
+R1 = maxpoolGlobal(2)
+R=convolucion(R1,Kn)
+print(R)
+print(R.shape)
+cv2.imwrite('002C.jpg',R)
