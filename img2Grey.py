dc61a64d365de04fa5cda68bb402e3977e9e8b56
SergioGonzalez24 <sergiomanuel2020@gmail.com>
2021-03-19 09:47:03 -0600

    Modificacion en codigo img2Grey

--- img2Grey.py
+++ img2Grey.py
@@ -2,11 +2,25 @@ import cv2
 import numpy as np
 
 #Cargar la imagen a color
-IRGB=cv2.imread('004.jpg')
-print(IRGB)
-print(IRGB.shape)
-print("Lineas agregadas en la rama2")
-IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
-print(IGS)
-print(IGS.shape)
-cv2.imwrite('004GS.jpg',IGS)
+IRGB_1 = cv2.imread('002.jpg')
+IGRGB_2 = cv2.imread("003.jpg")
+IRGB_3 = cv2.imread("008.jpg")
+IRGB_4 = cv2.imread("011.jpg")
+
+def img2Grey(imagen,nombreArchvioGrey):
+
+    print(imagen)
+    print(imagen.shape)
+    print("Lineas agregadas en la rama2")
+    IGS=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+    print(IGS)
+    print(IGS.shape)
+    cv2.imwrite(NombreArchivoGrey,IGS)
+
+
+img2Grey(IRGB_1,"002GS.jpg")
+img2Grey(IGRGB_2,"003GS.jpg")
+img2Grey(IRGB_3,"008GS.jpg")
+img2Grey(IRGB_4,"011GS.jpg")
+
+
