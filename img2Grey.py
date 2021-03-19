5ed975187e09ee26e2146486d6dc6ed93f87bb07
SergioGonzalez24 <sergiomanuel2020@gmail.com>
2021-03-19 10:20:54 -0600

    Actualizacion de Codigo img2Grey.py y generacion de imagenes en Escala de Grises

--- img2Grey.py
+++ img2Grey.py
@@ -1,26 +1,22 @@
 import cv2
 import numpy as np
 
-#Cargar la imagen a color
-IRGB_1 = cv2.imread('002.jpg')
-IGRGB_2 = cv2.imread("003.jpg")
-IRGB_3 = cv2.imread("008.jpg")
-IRGB_4 = cv2.imread("011.jpg")
+def img2Grey(original,nombre):
 
-def img2Grey(imagen,nombreArchvioGrey):
-
-    print(imagen)
-    print(imagen.shape)
+    IRGB= cv2.imread(original)
+    print(IRGB)
+    print(IRGB.shape)
     print("Lineas agregadas en la rama2")
-    IGS=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+    IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
     print(IGS)
     print(IGS.shape)
-    cv2.imwrite(NombreArchivoGrey,IGS)
+    return cv2.imwrite(nombre,IGS)
 
 
-img2Grey(IRGB_1,"002GS.jpg")
-img2Grey(IGRGB_2,"003GS.jpg")
-img2Grey(IRGB_3,"008GS.jpg")
-img2Grey(IRGB_4,"011GS.jpg")
 
+
+img2Grey("002.jpeg","002GS.jpeg")
+img2Grey("003.jpeg","003GS.jpeg")
+img2Grey("008.jpeg","008GS.jpeg")
+img2Grey("011.jpeg","011GS.jpeg")
 
