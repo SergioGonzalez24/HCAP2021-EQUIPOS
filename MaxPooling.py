e79b1e5d8a36ebcb8ebc145398ed4027d807df60
AlexisCastaneda <A01750119@itesm.mx>
2021-03-19 09:31:50 -0600

    Version 2 de MaxPooling

--- MaxPooling.py
+++ MaxPooling.py
@@ -4,3 +4,7 @@ def MaxPoolingDos(Img):
     Resultado=np.zeros((fr,cr),np.uint8)
     
     #Proceso del maxPooling
+    for i in range3(0,len(Img),2):
+        for j in range3(0,len(Img),2):
+            I[i:j,i:j]
+
