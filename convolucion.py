853d60c87863f561c44cf3b818c81e211200423b
JessZetina <80496363+JessZetina@users.noreply.github.com>
2021-03-19 22:51:43 -0600

    Update convolucion.py

--- convolucion.py
+++ convolucion.py
@@ -89,3 +89,159 @@ cv2.imwrite('002C2.jpeg',R1Conv2)
 ImgC_1_2 = cv2.imread('002C2.jpeg')
 R1Max2 = MaxPoolingDos(ImgC_1_2)
 print(R1Max2.shape,"\n")
+
+#Imagen 2 003GS.jpeg
+
+#funcion de convolucion con Kn
+print("\n")
+print("___________________________","\n","Imagen 1 003.jpeg Kernel 1")
+
+
+print(Img.shape,"\n")
+IGS=cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
+
+R2Conv1 = convolucion(IGS,Kn)
+print(R2Conv2)
+print(R2Conv2.shape,"\n")
+cv2.imwrite('003C1.jpeg',R2Conv2)
+
+ImgC_2_1 = cv2.imread('002C1.jpeg')
+R1Max1 = MaxPoolingDos(ImgC_2_1)
+print(R2Max1.shape,"\n")
+
+R2Conv2 = convolucion(R2Max2,Kn)
+print(R2Conv2,R2Conv2.shape, sep = "\n")
+cv2.imwrite('003C2.jpeg',R2Conv2)
+
+ImgC_2_2 = cv2.imread('003C2.jpeg')
+R2Max2 = MaxPoolingDos(ImgC_2_2)
+print(R2Max2.shape,"\n")
+
+#funcion de convolucion con In
+
+print("\n")
+print("___________________________","\n","Imagen 1 003.jpeg Kernel 2")
+
+print(Img.shape,"\n")
+IGS=cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
+
+R2Conv2 = convolucion(IGS,In)
+print(R2Conv2)
+print(R2Conv2.shape,"\n")
+cv2.imwrite('003C1.jpeg',R2Conv2)
+
+ImgC_2_1 = cv2.imread('003C1.jpeg')
+R2Max2 = MaxPoolingDos(ImgC_2_1)
+print(R2Max2.shape,"\n")
+
+R2Conv2 = convolucion(R2Max2,In)
+print(R2Conv1,R2Conv2.shape, sep = "\n")
+cv2.imwrite('003C2.jpeg',R2Conv2)
+
+ImgC_2_2 = cv2.imread('003C2.jpeg')
+R2Max2 = MaxPoolingDos(ImgC_2_2)
+print(R2Max2.shape,"\n")
+
+#Imagen 3 008GS.jpeg
+
+#funcion de convolucion con Kn
+print("\n")
+print("___________________________","\n","Imagen 1 008.jpeg Kernel 1")
+
+
+print(Img.shape,"\n")
+IGS=cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
+
+R3Conv1 = convolucion(IGS,Kn)
+print(R3Conv1)
+print(R3Conv1.shape,"\n")
+cv2.imwrite('002C1.jpeg',R3Conv1)
+
+ImgC_3_1 = cv2.imread('008C1.jpeg')
+R3Max1 = MaxPoolingDos(ImgC_3_1)
+print(R3Max1.shape,"\n")
+
+R3Conv2 = convolucion(R3Max1,Kn)
+print(R3Conv2,R3Conv2.shape, sep = "\n")
+cv2.imwrite('002C2.jpeg',R3Conv2)
+
+ImgC_3_2 = cv2.imread('008C2.jpeg')
+R3Max2 = MaxPoolingDos(ImgC_3_2)
+print(R3Max2.shape,"\n")
+
+#funcion de convolucion con In
+
+print("\n")
+print("___________________________","\n","Imagen 1 008.jpeg Kernel 2")
+
+print(Img.shape,"\n")
+IGS=cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
+
+R3Conv1 = convolucion(IGS,In)
+print(R3Conv1)
+print(R3Conv1.shape,"\n")
+cv2.imwrite('002C1.jpeg',R3Conv1)
+
+ImgC_3_1 = cv2.imread('008C1.jpeg')
+R3Max1 = MaxPoolingDos(ImgC_3_1)
+print(R3Max1.shape,"\n")
+
+R3Conv2 = convolucion(R3Max1,In)
+print(R3Conv2,R3Conv2.shape, sep = "\n")
+cv2.imwrite('002C2.jpeg',R3Conv2)
+
+ImgC_3_2 = cv2.imread('008C2.jpeg')
+R3Max2 = MaxPoolingDos(ImgC_3_2)
+print(R3Max2.shape,"\n")
+
+#Imagen 4 011GS.jpeg
+
+#funcion de convolucion con Kn
+print("\n")
+print("___________________________","\n","Imagen 4 011.jpeg Kernel 1")
+
+
+print(Img.shape,"\n")
+IGS=cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
+
+R4Conv1 = convolucion(IGS,Kn)
+print(R4Conv1)
+print(R4Conv1.shape,"\n")
+cv2.imwrite('011C1.jpeg',R4Conv1)
+
+ImgC_4_1 = cv2.imread('011C1.jpeg')
+R4Max1 = MaxPoolingDos(ImgC_4_1)
+print(R4Max1.shape,"\n")
+
+R4Conv2 = convolucion(R4Max1,Kn)
+print(R4Conv2,R4Conv2.shape, sep = "\n")
+cv2.imwrite('002C2.jpeg',R4Conv2)
+
+ImgC_4_2 = cv2.imread('011C2.jpeg')
+R4Max2 = MaxPoolingDos(ImgC_4_2)
+print(R4Max2.shape,"\n")
+
+#funcion de convolucion con In
+
+print("\n")
+print("___________________________","\n","Imagen 4 011.jpeg Kernel 2")
+
+print(Img.shape,"\n")
+IGS=cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
+
+R4Conv1 = convolucion(IGS,In)
+print(R4Conv1)
+print(R4Conv1.shape,"\n")
+cv2.imwrite('011C1.jpeg',R4Conv1)
+
+ImgC_4_1 = cv2.imread('011C1.jpeg')
+R4Max1 = MaxPoolingDos(ImgC_4_1)
+print(R4Max1.shape,"\n")
+
+R4Conv2 = convolucion(R4Max1,In)
+print(R4Conv2,R4Conv2.shape, sep = "\n")
+cv2.imwrite('011C2.jpeg',R4Conv2)
+
+ImgC_4_2 = cv2.imread('011C2.jpeg')
+R4Max2 = MaxPoolingDos(ImgC_4_2)
+print(R4Max2.shape,"\n")
